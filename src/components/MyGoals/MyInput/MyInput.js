18e9d24a17bd8cb39d140765af293c3d9e60daca
AdmiralSultano <giordano2010@gmail.com>
2021-12-28 16:44:57 +0700

    Setting css files dynamically

--- src/components/MyGoals/MyInput/MyInput.js
+++ src/components/MyGoals/MyInput/MyInput.js
@@ -26,16 +26,9 @@ const MyInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label style={{ color: !isValid ? "red" : "black" }}>My Goal</label>
-        <input
-          style={{
-            borderColor: !isValid ? "red" : "black",
-            background: !isValid ? "salmon" : "transparent",
-          }}
-          type="text"
-          onChange={goalInputChangeHandler}
-        />
+      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+        <label>My Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
